c99a82daff15d98a0c67207bdceeaa58dc4e1d26
Howard <howard.huang@mail.mcgill.ca>
2019-03-15 08:44:43 +0000

    Setup

--- setup.sh
+++ setup.sh
@@ -3,10 +3,18 @@
 git clone https://github.com/temporaryorgname/tracker-backend.git
 git clone https://github.com/temporaryorgname/tracker-frontend.git
 
+# Install required packages
+apt-get update
+apt-get --assume-yes install npm python3 virtualenv
+
+# Setup repo
 cd tracker-frontend
 npm install
 npm run build
 cd ../tracker-backend/fitnessapp
-mkdir static
+virtualenv ENV
+source ENV/bin/activate
+pip3 install -r requirements.txt
+mkdir -p static
 cd static
 cp -r ../../../tracker-frontend/build/* .
