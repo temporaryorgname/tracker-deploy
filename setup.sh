3622b6369e27da27e22bcc04af9ca6a9607f65a8
Howard <howard.huang@mail.mcgill.ca>
2019-04-13 01:27:41 +0000

    Download assets

--- setup.sh
+++ setup.sh
@@ -2,8 +2,13 @@
 
 git clone https://github.com/temporaryorgname/tracker-backend.git
 git clone https://github.com/temporaryorgname/tracker-frontend.git
+git clone https://github.com/temporaryorgname/assets.git
 
 cd tracker-frontend
+cd src
+mkdir images
+cp ../../assets/background.jpeg images
+cd ..
 npm install
 npm run build
 cd ../tracker-backend/fitnessapp
